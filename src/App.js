43f49a7c53eeb75657319f369714e732a6f35717
halvimkurk <halvimkurk@yandex.ru>
2020-10-28 10:50:26 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import './App.scss'
+import Navbar from './components/Navbar'
+import TourList from './components/TourList'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <TourList />
+    </>
+    
+  );
+}
+
+export default App;
